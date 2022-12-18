e62edc817a8b75e401d6c8a0a66243d009c31fbd
pgrugnale <pgrugnale@arrivelogistics.com>
2022-12-17 23:26:35 -0600

    day 15 pt2

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,36 +1,64 @@
 import Position.Companion.manhattanDistance
+import java.lang.Integer.max
+import java.lang.Integer.min
 import kotlin.math.abs
 
 fun main() {
+    fun positionsWithoutBeaconForRow(input: List<Position>, row: Int): List<IntRange> {
+        val sensorsAffectingRow = input.filter {
+            abs(it.sensorY - row) <= it.manhattanDistance()
+        }
+        val xRanges = sensorsAffectingRow.map {
+            val (xMin, xMax) = funMath(it.sensorX, it.sensorY, row, it.manhattanDistance())
+            Pair(xMin, xMax)
+        }.sortedBy { it.first }
+
+        return xRanges.fold(mutableListOf<Pair<Int, Int>>()) { acc, item ->
+            if (acc.isEmpty() || acc.last().second < item.first) {
+                acc.add(item)
+            } else {
+                val last = acc.removeLast()
+                acc.add(Pair(min(item.first, last.first), max(item.second, last.second)))
+            }
+            acc
+        }.map { IntRange(it.first, it.second) }
+    }
+
     fun part1(input: List<Position>, row: Int): Int {
         val sensorsAffectingRow = input.filter {
             abs(it.sensorY - row) <= it.manhattanDistance()
         }
-        val possiblePositions = sensorsAffectingRow.map {
-            val (xMin, xMax) = funMath(it.sensorX, it.sensorY, row, it.manhattanDistance())
-            IntRange(xMin, xMax).toSet()
-        }.reduce { a, b -> a.union(b) }
 
-        val beaconsInRow = sensorsAffectingRow.filter {
-            it.beaconY == row
-        }.map {
-            it.beaconX
-        }.toSet()
+        val rangesWithoutBeacon = positionsWithoutBeaconForRow(input, row)
+        val beaconsInRow = sensorsAffectingRow.filter {it.beaconY == row }.map { it.beaconX }.distinct()
+            .count { beacon ->
+                rangesWithoutBeacon.map { beacon in it }.any { it }
+            }
 
-        return (possiblePositions subtract beaconsInRow).size
+        return rangesWithoutBeacon.sumOf { it.count() } - beaconsInRow
     }
 
-    fun part2(input: List<Position>): Int {
+    fun part2(input: List<Position>, validRange: IntRange): Long {
 
-//        (0..4000000).first {
-//            part1(input, it)
-//        }
-        return input.size
+        var emptyX = -1
+        val answerY = validRange.asSequence().find { row ->
+            val rangesWithoutBeacon = positionsWithoutBeaconForRow(input, row)
+            val beaconsInRow = input.filter { it.beaconY == row}.map { it.beaconX }
+            rangesWithoutBeacon.map {range ->
+                if ((range.first - 1) in validRange && (range.first - 1) !in beaconsInRow) {
+                    emptyX = range.first - 1
+                    true
+                } else {
+                    false
+                }
+            }.any { it }
+        }
+        println("row found at y=$answerY with empty X space at x=$emptyX")
+        return emptyX * 4000000L + answerY!!
     }
 
     // test if implementation meets criteria from the description, like:
     //val input = readInput("Day15_test")
-    //check(part1(testInput) == 1)
 
     val input = readInput("Day15")
     val regex = Regex("x=(-?\\d+), y=(-?\\d+)")
@@ -44,7 +72,12 @@ fun main() {
             matchResults[1].groups[2]!!.value.toInt()
         )
     }
-    println(part1(coordinates, 2000000))
+
+    println("part 1 solution: ${part1(coordinates, 2000000)}")
+    println("part 2 solution: ${part2(coordinates, 0..4000000)}")
+//    println("part 1 solution: ${part1(coordinates, 10)}")
+//    println("part 2 solution: ${part2(coordinates, 0..20)}")
+
 }
 
 data class Position(
